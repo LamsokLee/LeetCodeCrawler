6ae7096cc1849b550c52cb4e292ca47ee2bb804d
linshuoli <lilinshuo1110@gmail.com>
2019-10-23 21:21:32 -0400

    force gc

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -8,9 +8,7 @@ const start = async() => {
         result = await request(config.problem_list_api);   
         json = JSON.parse(result);
         console.log("Problem list loaded: " + json['stat_status_pairs'].length + " problems");
-        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
         await updateDataBase(json);
-        await browser.close();
     } catch (e){
         console.log(e);
     }
@@ -41,6 +39,7 @@ const updateDataBase = async(json) => {
             }
         });
         console.log('Question updated: ' + id);
+        global.gc();
     }
 }
 
@@ -50,6 +49,7 @@ const getLikeAndDislikeCount = async(question_title) => {
     var dislikes;
     try {
     problem_url = config.problem_base + question_title;
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.setViewport({
         width: 1920,
@@ -67,6 +67,9 @@ const getLikeAndDislikeCount = async(question_title) => {
         return document.querySelectorAll('.btn__r7r7')[1].querySelector('span').innerHTML;
     });
     console.log("likes: " + likes + " dislikes: " + dislikes);
+
+    await browser.close();
+
   } catch (error) {
     console.log(error);
   }
