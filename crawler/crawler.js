418bcd04255c9682f57962b99bbd7d3b37b4384e
Linshuo Li <lli@karenclarkandco.com>
2019-10-24 11:42:25 -0400

    close page

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -4,21 +4,21 @@ const config = require('../config/url');
 const puppeteer = require('puppeteer');
 var browser;
 
-const start = async() => {
+const start = async () => {
     try {
-        browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-        result = await request(config.problem_list_api);   
+        browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        result = await request(config.problem_list_api);
         json = JSON.parse(result);
         console.log("Problem list loaded: " + json['stat_status_pairs'].length + " problems");
         await updateDataBase(json);
         await browser.close();
 
-    } catch (e){
+    } catch (e) {
         console.log(e);
     }
 }
 
-const updateDataBase = async(json) => {
+const updateDataBase = async (json) => {
     for (i = 0; i < json['stat_status_pairs'].length; i++) {
         var problems = json['stat_status_pairs'];
         var stat = problems[i]['stat'];
@@ -26,17 +26,17 @@ const updateDataBase = async(json) => {
         var like_dislike = await getLikeAndDislikeCount(stat['question__title_slug']);
         stat['like_count'] = like_dislike[0];
         stat['dislike_count'] = like_dislike[1];
-        
-        var options = {upsert: true};
-        
-        Problem.updateOne({ question_id : stat['question_id']}, {
-            question__title : stat['question__title'],
-            question__title_slug : stat['question__title_slug'],
-            total_acs : stat['total_acs'],
-            total_submitted : stat['total_submitted'],
-            difficulty : problems[i]['difficulty']['level'],
-            like_count : stat['like_count'],
-            dislike_count : stat['dislike_count']
+
+        var options = { upsert: true };
+
+        Problem.updateOne({ question_id: stat['question_id'] }, {
+            question__title: stat['question__title'],
+            question__title_slug: stat['question__title_slug'],
+            total_acs: stat['total_acs'],
+            total_submitted: stat['total_submitted'],
+            difficulty: problems[i]['difficulty']['level'],
+            like_count: stat['like_count'],
+            dislike_count: stat['dislike_count']
         }, options, (err) => {
             if (err) {
                 console.error(err);
@@ -46,35 +46,33 @@ const updateDataBase = async(json) => {
     }
 }
 
-const getLikeAndDislikeCount = async(question_title) => {
+const getLikeAndDislikeCount = async (question_title) => {
     console.log('Opening problem page: ' + question_title);
-    var likes;
-    var dislikes;
-    try {
-    problem_url = config.problem_base + question_title;
     const page = await browser.newPage();
-    await page.setViewport({
-        width: 1920,
-        height: 1080
-    });
-    await page.goto(problem_url);
-    
-    await page.waitForSelector('#initial-loading', { hidden : true });
+    try {
+        problem_url = config.problem_base + question_title;
+        await page.setViewport({
+            width: 1920,
+            height: 1080
+        });
+        await page.goto(problem_url);
 
-    likes = await page.evaluate(() => {
-        return document.querySelectorAll('.btn__r7r7')[0].querySelector('span').innerHTML;
-    });
+        await page.waitForSelector('#initial-loading', { hidden: true });
 
-    dislikes = await page.evaluate(() => {
-        return document.querySelectorAll('.btn__r7r7')[1].querySelector('span').innerHTML;
-    });
-    console.log("likes: " + likes + " dislikes: " + dislikes);
+        var likes = await page.evaluate(() => {
+            return document.querySelectorAll('.btn__r7r7')[0].querySelector('span').innerHTML;
+        });
 
-
-  } catch (error) {
-    console.log(error);
-  }
-  return [likes, dislikes];
+        var dislikes = await page.evaluate(() => {
+            return document.querySelectorAll('.btn__r7r7')[1].querySelector('span').innerHTML;
+        });
+        console.log("likes: " + likes + " dislikes: " + dislikes);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        page.close();
+    }
+    return [likes, dislikes];
 };
 
 start();
