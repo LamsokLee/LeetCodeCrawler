42e06a374a87e6e2aaefc59f953c1b54d9860e32
Linshuo Li <lli@karenclarkandco.com>
2019-10-24 20:06:03 -0400

    added api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const Problem = require('../crawler/database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/api/v1/question/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  Problem.findOne({question_id : id})
+  .select('-_id question_id question__title question__title_slug total_acs total_submitted like_count dislike_count difficulty')
+  .exec((err, problem) => {
+    if (problem) {
+      res.json(problem);
+    } else {
+      res.json({ message: "Question " + id + " doesn't exist"});
+    }
+  });
+})
 
 module.exports = router;
