24d0d869c5fe20dd44324e1163bbe26fb2f4c959
linshuoli <lilinshuo1110@gmail.com>
2019-10-25 22:11:43 -0400

    changed port

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,8 @@
 const proxy = require("http-proxy-middleware");
+var port = process.env.PORT || 5000;
 
 module.exports = function(app) {
   app.use(
-    proxy(["/api", , "/otherApi"], { target: "http://localhost:5000" })
+    proxy(["/api", , "/otherApi"], { target: "http://localhost:" + port   })
   );
 };
