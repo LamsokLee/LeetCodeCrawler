fd8fddb774f1521fdf682942d3cf36a95665c528
Linshuo Li <lli@karenclarkandco.com>
2019-10-29 17:59:10 -0400

    added sortable table

--- client/src/components/Ranking.js
+++ client/src/components/Ranking.js
@@ -6,6 +6,7 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import Table from 'react-bootstrap/Table'
 import Container from 'react-bootstrap/Container'
 import { Link } from 'react-router-dom';
+import { number } from 'prop-types';
 
 
 class Ranking extends Component {
@@ -16,28 +17,48 @@ class Ranking extends Component {
             problemList: [],
             columns : [{
                 dataField : 'question_id',
-                text : 'Question ID'
+                text : 'Question ID',
+                type : number,
+                sort: true
             }, {
                 dataField : 'question__title',
                 text : 'Question Name'
             }, {
                 dataField : 'total_submitted',
-                text : 'Submission'
+                text : 'Submission',
+                type : number,
+                sort: true
             }, {
                 dataField : 'total_acs',
-                text : 'Accept'
+                text : 'Accept',
+                type : number,
+                sort: true
             }, {
-                dataField : 'acceptance_rate',
-                text : 'Acceptance Rate'
+                dataField : "acceptance_rate.$numberDecimal",
+                text : 'Acceptance Rate (%)',
+                type : number,
+                sort: true,
+                formatter : (cell, row) => {
+                    return (cell * 100).toFixed(2);
+                }
             }, {
                 dataField : 'like_count',
-                text : 'Liked'
+                text : 'Liked',
+                type : number,
+                sort: true
             }, {
                 dataField : 'dislike_count',
-                text : 'Disliked'
+                text : 'Disliked',
+                type : number,
+                sort: true
             }, {
-                dataField : 'like_rate',
-                text : 'Like Rate'
+                dataField : "like_rate.$numberDecimal",
+                text : 'Like Rate (%)',
+                type : number,
+                sort: true,
+                formatter : (cell, row) => {
+                    return (cell * 100).toFixed(2);
+                }
             }]           
         };
 
@@ -50,48 +71,48 @@ class Ranking extends Component {
         .then(l => this.setState({ problemList : l}));
     }
 
-    // render() {
-    //     return (
-    //         <BootstrapTable keyField = 'question_id' data = {this.state.problemList} columns = {this.state.columns}/>
-    //     )
-    // }
     render() {
         return (
-            <Container>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                        <th>Question ID</th>
-                        <th>Question Name</th>
-                        <th>Submission</th>
-                        <th>Accept</th>
-                        <th>Acceptance Rate</th>
-                        <th>Liked</th>
-                        <th>Disliked</th>
-                        <th>Liked Rate</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.problemList.map(q => (
-                                <tr>
-                                <td>{q.question_id}</td>
-                                <td>{q.question__title}</td>
-                                <td>{q.total_submitted}</td>
-                                <td>{q.total_acs}</td>
-                                <td>{q.acceptance_rate == null ? 0 : q.acceptance_rate['$numberDecimal']}</td>
-                                <td>{q.like_count}</td>
-                                <td>{q.dislike_count}</td>
-                                <td>{q.like_rate == null ? 0 : q.like_rate['$numberDecimal']}</td>
-                                </tr>
-                            ))
-                        }
-                        
-                    </tbody>
-                </Table>
-            </Container>
-        );
+            <BootstrapTable bootstrap4 = 'true' keyField = 'question_id' data = {this.state.problemList} columns = {this.state.columns}/>
+        )
     }
+    // render() {
+    //     return (
+    //         <Container>
+    //             <Table striped bordered hover>
+    //                 <thead>
+    //                     <tr>
+    //                     <th>Question ID</th>
+    //                     <th>Question Name</th>
+    //                     <th>Submission</th>
+    //                     <th>Accept</th>
+    //                     <th>Acceptance Rate</th>
+    //                     <th>Liked</th>
+    //                     <th>Disliked</th>
+    //                     <th>Liked Rate</th>
+    //                     </tr>
+    //                 </thead>
+    //                 <tbody>
+    //                     {
+    //                         this.state.problemList.map(q => (
+    //                             <tr>
+    //                             <td>{q.question_id}</td>
+    //                             <td>{q.question__title}</td>
+    //                             <td>{q.total_submitted}</td>
+    //                             <td>{q.total_acs}</td>
+    //                             <td>{q.acceptance_rate == null ? 0 : q.acceptance_rate['$numberDecimal']}</td>
+    //                             <td>{q.like_count}</td>
+    //                             <td>{q.dislike_count}</td>
+    //                             <td>{q.like_rate == null ? 0 : q.like_rate['$numberDecimal']}</td>
+    //                             </tr>
+    //                         ))
+    //                     }
+                        
+    //                 </tbody>
+    //             </Table>
+    //         </Container>
+    //     );
+    // }
 }
 
 export default Ranking;
