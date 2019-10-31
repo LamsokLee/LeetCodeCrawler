0bf22c262f11a5302feee1e167155ec131af9b7f
Linshuo Li <lli@karenclarkandco.com>
2019-10-31 14:40:15 -0400

    Table style changes, added more rows

--- client/src/components/Ranking.js
+++ client/src/components/Ranking.js
@@ -3,32 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
-import filterFactory, { textFilter, numberFilter, Comparator } from 'react-bootstrap-table2-filter';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 import { number } from 'prop-types';
-import Container from 'react-bootstrap/Container';
+import { Container } from 'react-bootstrap';
 
 
 const defaultSorted = [{
-    dataField: 'question_id', 
+    dataField: 'question_id',
     order: 'asc'
 }];
 
 class ProblemLinkFormatter extends React.Component {
     render() {
         return (
-        <a href = {this.props.link}> {this.props.title} </a>
+            <a href={this.props.link}> {this.props.title} </a>
         );
     }
 }
 
-var rateFilter = numberFilter({
-    options: [0.3, 0.5, 0.8],
-    comparators: [Comparator.GT, Comparator.LT],
-    withoutEmptyComparatorOption: true,
-    style: { display: 'inline-grid' },
-    comparatorClassName: 'custom-comparator-class',
-    numberClassName: 'custom-number-class'
-});
 
 class Ranking extends Component {
     constructor(props) {
@@ -36,75 +28,104 @@ class Ranking extends Component {
 
         this.state = {
             problemList: [],
-            columns : [{
-                dataField : 'question_id',
-                text : 'UID',
-                type : number,
+            columns: [{
+                dataField: 'question_id',
+                text: 'UID',
+                style: { width : '50%' },
+                type: number,
                 filter: textFilter(),
-                hidden : true,
-                headerStyle : (colum, colIndex) => {
-                    return {
-                    };
-                },
-                sort: true
-            },{
-                dataField : 'frontend_id',
-                text : 'ID',
-                type : number,
-                filter: textFilter(),
+                hidden: true,
                 sort: true
             }, {
-                dataField : 'question__title',
-                text : 'Name',
-                filter : textFilter(),
-                headerStyle : (colum, colIndex) => {
-                    return { 
+                dataField: 'frontend_id',
+                text: 'ID',
+                type: number,
+                filter: textFilter({
+                    style: {  }
+                }),
+                sort: true
+            }, {
+                dataField: 'question__title',
+                text: 'Name',
+                sort: true,
+                filter: textFilter({
+                    placeholder: '',
+                    style: { width : '100%' }
+                }),
+                headerStyle: (colum, colIndex) => {
+                    return {
                         'width': '30%'
                     };
                 },
-                formatter : (cell, row, rowIndex) => {
+                formatter: (cell, row, rowIndex) => {
                     var url = "https://leetcode.com/problems/" + row['question__title_slug'];
                     return (
-                        <ProblemLinkFormatter link = {url} title = {cell}> cell </ProblemLinkFormatter>
+                        <ProblemLinkFormatter link={url} title={cell}> cell </ProblemLinkFormatter>
                     )
                 }
             }, {
-                dataField : 'total_submitted',
-                text : 'Submission',
-                type : number,
-                sort: true
-            }, {
-                dataField : 'total_acs',
-                text : 'Accept',
-                type : number,
-                sort: true
-            }, {
-                dataField : "acceptance_rate.$numberDecimal",
-                text : 'Acceptance Rate (%)',
-                type : number,
+                dataField: 'total_submitted',
+                text: 'Submission',
+                type: number,
                 sort: true,
-                formatter : (cell, row) => {
-                    return (cell * 100).toFixed(2);
+                classes: 'table-holder',
+                style: {
+                    'overflow-x':'auto'
                 }
             }, {
-                dataField : 'like_count',
-                text : 'Liked',
-                type : number,
+                dataField: 'total_acs',
+                text: 'Accept',
+                type: number,
                 sort: true
             }, {
-                dataField : 'dislike_count',
-                text : 'Disliked',
-                type : number,
-                sort: true
-            }, {
-                dataField : "like_rate.$numberDecimal",
-                text : 'Like Rate (%)',
-                type : number,
+                dataField: "acceptance_rate.$numberDecimal",
+                text: 'Acceptance Rate (%)',
+                type: number,
                 sort: true,
-                formatter : (cell, row) => {
-                    return (cell * 100).toFixed(2);
+                formatter: (cell, row) => {
+                    if (cell) {
+                        return (cell * 100).toFixed(2);
+                    } else {
+                        return "-"
+                    }
                 }
-            }]           
+            }, {
+                dataField: 'like_count',
+                text: 'Liked',
+                type: number,
+                sort: true,
+                formatter: (cell, row) => {
+                    if (cell) {
+                        return cell;
+                    } else {
+                        return "-";
+                    }
+                }
+            }, {
+                dataField: 'dislike_count',
+                text: 'Disliked',
+                type: number,
+                sort: true,
+                formatter: (cell, row) => {
+                    if (cell) {
+                        return cell;
+                    } else {
+                        return "-";
+                    }
+                }
+            }, {
+                dataField: "like_rate.$numberDecimal",
+                text: 'Like Rate (%)',
+                type: number,
+                sort: true,
+                formatter: (cell, row) => {
+                    if (cell) {
+                        return (cell * 100).toFixed(2);
+                    } else {
+                        return "-"
+                    }
+                }
+            }]
         };
 
         this.getAllProblems();
@@ -112,21 +133,26 @@ class Ranking extends Component {
 
     async getAllProblems() {
         fetch('/api/v1/ranking')
-        .then(response => response.json())
-        .then(l => this.setState({ problemList : l}));
+            .then(response => response.json())
+            .then(l => this.setState({ problemList: l }));
     }
 
     render() {
         return (
             <Container>
                 <BootstrapTable 
-                    keyField = 'question_id'
-                    data = {this.state.problemList}
-                    columns = {this.state.columns}
-                    pagination = { paginationFactory() }
-                    filter = { filterFactory() }
-                    defaultSorted = { defaultSorted }
+                    keyField='question_id'
+                    data={this.state.problemList}
+                    columns={this.state.columns}
+                    bootstrap4='true'
+                    pagination={paginationFactory({ paginationSize: 5, sizePerPage: 50, showTotal: true })}
+                    filter={filterFactory()}
+                    defaultSorted={defaultSorted}
+                    noDataIndication="No questions"
                     hover
+                    striped
+                    condensed
+                    bordered={false}
                 />
             </Container>
         )
